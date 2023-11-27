6e70e02577e91d0106e3f9e56953ab0c9b659d61
vlada <vladaray010@gmail.com>
2023-11-27 10:03:21 +0300

    .

---
+++ src/test/java/com/example/proba/ProbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
